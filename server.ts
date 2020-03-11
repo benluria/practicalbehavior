c460c167441b3d8b6e2a6a0a0e5bbddfaa3449d7
Benjamin Luria <benjamin.luria@A05808.local>
2020-03-10 22:05:09 -0700

    port for azure

--- server.ts
+++ server.ts
@@ -18,7 +18,7 @@ const app = express();
 app.use(express.urlencoded({extended: true})); 
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = process.env.PORT || 1337;
 const DIST_FOLDER = join(process.cwd(), 'dist');
 
 // * NOTE :: leave this as require() since this file is built Dynamically from webpack
